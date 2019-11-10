edeab62885667fbc12092d97f8d199560a7f5416
Ligiano Azzi <ldaazzi@gmail.com>
2019-11-10 19:42:07 +0000

    first commit - creating the project

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use App\Entity\Cliente;
+
+
+use App\Entity\Animal;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     * @Template("default/index.html.twig")
+     */
+    public function index()
+    {
+
+      $em = $this->getDoctrine()->getManager();
+
+      $qts_animais = $em->getRepository(Cliente::class)->qtsAnimaisPorCliente();
+
+      $qte_racas = $em->getRepository(Animal::class)->qtsPorRaca();
+
+
+    	return [
+        'qts_animais' => $qts_animais,
+        'qtde_por_raca' => $qte_racas
+      ];
+    }
+}
---
+++ src/Repository/ClienteRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Cliente;
+use PDO;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Cliente|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Cliente|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Cliente[]    findAll()
+ * @method Cliente[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ClienteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Cliente::class);
+    }
+
+    public function qtsAnimaisPorCliente()
+    {
+       $sql = "select COUNT(ac.animal_id) as qtde,
+               c.nome FROM animal_cliente ac INNER JOIN
+               cliente c ON c.id = ac.cliente_id
+               GROUP BY c.nome
+       ";
+       return $this->getEntityManager()->getConnection()->executeQuery($sql)
+           ->fetchAll(PDO::FETCH_OBJ);
+    }
+
+
+    // /**
+    //  * @return Cliente[] Returns an array of Cliente objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Cliente
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
